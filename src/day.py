9955cee6fa75ec5b8e317cbfb2bfd756a644c66f
Macuyler Dunn <macuyler@protonmail.com>
2022-04-20 22:19:20 -0500

    Add day string formatting

--- src/day.py
+++ src/day.py
@@ -1,10 +1,42 @@
-"""A log line representing a single day."""
+"""A log line representing a single day.
+
+* Example day line:
+04/20/22=05:43
+"""
 
 import datetime
+import re
+from typing import Optional
 
+DATE_FORMAT = "%m/%d/%y"
+LOG_FORMAT = re.compile(r'\d\d/\d\d/\d\d=\d\d:\d\d')
 
 class Day:
 
     def __init__(self, date:datetime.date, time:float):
         self.date = date
         self.time = time
+
+
+    def __str__(self):
+        date_str = self.date.strftime(DATE_FORMAT)
+        hours_str = str(int(self.time)).rjust(2, '0')
+        mins_str = str(int(self.time * 100) % 100)
+
+        return f'{date_str}={hours_str}:{mins_str}'
+
+
+def day(string:str) -> Optional[Day]:
+    """Attempt to convert a string to a Day."""
+
+    out = None
+    string = string.strip()
+
+    if LOG_FORMAT.match(string):
+        date_str, time_str = string.split('=')
+        hours, mins = time_str.split(':')
+        time = float(hours) + float(mins) / 60
+        date = datetime.datetime.strptime(date_str, DATE_FORMAT).date()
+        out = Day(date, time)
+
+    return out
--- tests/day.py
+++ tests/day.py
@@ -3,18 +3,34 @@
 import datetime
 import unittest
 
-from src.day import Day
+from src.day import Day, day
 
 
 class TestDay(unittest.TestCase):
 
-    def test_stuff(self):
+    def test_day_to_string(self):
+        msg = 'Days should be formatted when converted to a string.'
 
-        today = datetime.date.today()
-        day = Day(today, 1.0)
-        print(day)
+        result = str(Day(datetime.date(2020, 12, 31), 12.35))
+        expected = '12/31/20=12:35'
+        self.assertEqual(result, expected, msg)
 
-        self.assertEqual(0, 0)
+        result = str(Day(datetime.date(2000, 2, 1), 1.10001))
+        expected = '02/01/00=01:10'
+        self.assertEqual(result, expected, msg)
+
+
+    def test_string_to_day(self):
+        result = day('04/20/22=01:00')
+        expected = Day(datetime.date(2022, 4, 20), 1)
+        msg = 'Strings should be parsed into Days.'
+        self.assertEqual(result.date, expected.date, msg)
+        self.assertEqual(result.time, expected.time, msg)
+
+        result = day('wednesday')
+        expected = None
+        msg = 'None should be returned when parsing invalid strings.'
+        self.assertEqual(result, expected, msg)
 
 
 if __name__ == '__main__':
