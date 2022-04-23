9c9b3542f0f2207569fd6f6d1417402b6b4f3c3e
Macuyler Dunn <macuyler@protonmail.com>
2022-04-22 21:39:34 -0500

    Add basic log loading and parsing

--- src/log.py
+++ src/log.py
@@ -14,7 +14,12 @@
 Grand Total = 00:00
 """
 
+import datetime
 from pathlib import Path
+
+from src.day import Day, str_to_day
+from src.time import hmt
+from src.week import Week
 
 
 class Log:
@@ -26,8 +31,67 @@ class Log:
         self._load()
 
     def _load(self):
-        """Load existing log data."""
+        """Load and process existing log data."""
 
         with self.path.open('r', encoding='utf-8') as log_file:
             lines = log_file.readlines()
-            print(lines)
+
+        is_day = lambda x: not x is None
+        days = list(filter(is_day, map(str_to_day, lines)))
+        days.sort()
+        days = rectify(days)
+
+        week_length = 7
+        week_days = [ days[i:i+week_length] for i in range(0, len(days), week_length)]
+        # Ref: https://www.geeksforgeeks.org/break-list-chunks-size-n-python/
+
+        for i, week in enumerate(week_days):
+            # Convert days back to string?
+            week = list(map(str, week))
+
+            self.weeks.append(Week(i + 1, week))
+
+
+    def __str__(self):
+        return '\n'.join(map(str, self.weeks))
+
+
+def rectify(days:list[Day]) -> list[Day]:
+    """Combine duplicates and fill in missing days."""
+
+    carrier = hmt(0, 0)
+    new_days = []
+
+    for i, day in enumerate(days):
+        current = Day(day.date, day.time + carrier)
+
+        if i < len(days) - 1:
+            next_day = days[i + 1]
+
+            if next_day.date == day.date:
+                carrier += day.time
+                continue
+
+            new_days.append(current)
+            new_days += missing(day, next_day)
+            carrier = hmt(0, 0)
+
+        else:
+            new_days.append(current)
+
+    return new_days
+
+
+def missing(start:Day, end:Day) -> list[Day]:
+    """Create a list of all days between the given dates."""
+
+    one_day = datetime.timedelta(days=1)
+    gap = end.date - start.date
+    missing_days = []
+
+    while gap > one_day:
+        gap -= one_day
+        day = Day(end.date - gap, hmt(0, 0))
+        missing_days.append(day)
+
+    return missing_days
