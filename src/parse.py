680d487b9db681d201145212784c8804deb4c762
Macuyler Dunn <macuyler@protonmail.com>
2022-05-18 14:54:09 -0500

    Add custom string parser in src.parse

---
+++ src/parse.py
@@ -0,0 +1,129 @@
+"""A set of string parsing utilities."""
+
+from functools import reduce
+from typing import Callable, Optional, Union
+
+Parser = Callable[Optional[str], Optional[int]]
+Pattern = list[Union[str, Parser]]
+
+nop = lambda x: x
+
+
+def compose(*func: Callable) -> Callable:
+    """Compose a single function from the given set."""
+
+    # Ref: https://www.geeksforgeeks.org/function-composition-in-python/
+    merge = lambda f, g: lambda x: f(g(x))
+    return reduce(merge, func, nop)
+
+
+def check(condition: Callable[str, bool]) -> Parser:
+    """Return None if the given condition fails."""
+
+    return lambda x: x if x is None or condition(x) else None
+
+
+def int_parser(minimum: Optional[int] = None, maximum: Optional[int] = None) -> Parser:
+    """Create an integer parsing function with the given bounds."""
+
+    apply_min = check(lambda x: x >= minimum)
+    apply_max = check(lambda x: x <= maximum)
+
+    return compose(apply_max if maximum else nop,
+                   apply_min if minimum else nop,
+                   int)
+
+
+def get_pattern(fmt: str) -> Pattern:
+    """Generate a pattern from the given format string."""
+
+    fmt = list(fmt)
+    output = []
+
+    flags = {
+        'Y': int_parser(0, 99),
+        'M': int_parser(1, 12),
+        'D': int_parser(1, 31),
+        'h': int_parser(0),
+        'm': int_parser(0, 59),
+    }
+
+    flag = False
+    delim = ''
+
+    while len(fmt) > 0:
+        char = fmt.pop(0)
+
+        if flag and char in flags:
+            output.append(flags[char])
+            continue
+
+        if char == '%':
+            flag = True
+
+            if delim:
+                output.append(delim)
+                delim = ''
+
+        else:
+            delim += char
+
+    if delim:
+        output.append(delim)
+
+    return output
+
+
+def parse(pattern: Pattern, string: str) -> list[Optional[int]]:
+    """Parse a set of integers from the given value according to a pattern."""
+
+    prefix = pattern[0]
+    if isinstance(prefix, str) and string.startswith(prefix):
+        string = string[len(prefix):]
+        pattern = pattern[1:]
+
+    suffix = pattern[-1]
+    if isinstance(suffix, str) and string.endswith(suffix):
+        string = string[:len(string) - len(suffix)]
+        pattern = pattern[:-1]
+
+    output = []
+    string = list(string)
+
+    handle = None
+    delim = None
+
+    for i, item in enumerate(pattern):
+        if callable(item):
+            handle = item
+        elif isinstance(item, str):
+            delim = item
+
+        delim_check = ''
+        value = ''
+
+        if delim:
+            while delim_check != delim and len(string) > 0:
+                char = string.pop(0)
+
+                if delim.startswith(delim_check + char):
+                    delim_check += char
+                else:
+                    value += char
+
+            delim = None
+
+        if delim is None and i == len(pattern) - 1:
+            value = ''.join(string)
+
+        if handle and value:
+            try:
+                output.append(handle(value))
+            except ValueError:
+                output.append(None)
+
+    delim_count = len(list(filter(lambda x: isinstance(x, str), pattern)))
+    while len(output) < delim_count + 1:
+        output.append(None)
+
+    return output
---
+++ tests/test_parse.py
@@ -0,0 +1,128 @@
+"""Unit tests covering the src.parse module."""
+
+import unittest
+from typing import Callable
+
+from src.parse import get_pattern, int_parser, parse
+
+
+class TestParse(unittest.TestCase):
+
+    def test_get_pattern(self):
+        fmt = '%M/%D/%Y'
+        result = get_pattern(fmt)
+        expected = [int_parser(1, 12), '/', int_parser(1, 31), '/', int_parser(0, 99)]
+        msg = 'Format strings should be parsed to generate a pattern.'
+        self.assertTrue(parsers_match(result[0], expected[0], range(0, 14)), msg)
+        self.assertEqual(result[1], expected[1], msg)
+        self.assertTrue(parsers_match(result[2], expected[2], range(0, 33)), msg)
+        self.assertEqual(result[3], expected[3], msg)
+        self.assertTrue(parsers_match(result[4], expected[4], range(-1, 101)), msg)
+
+
+    def test_pattern_prefix(self):
+        fmt = 'Prefix: %M/%D/%Y'
+        result = get_pattern(fmt)
+        expected = ['Prefix: ', int_parser(1, 12), '/', int_parser(1, 31), '/', int_parser(0, 99)]
+        msg = 'Format parsing should retain any prefix content.'
+        self.assertEqual(result[0], expected[0], msg)
+        self.assertTrue(parsers_match(result[1], expected[1], range(0, 14)), msg)
+        self.assertEqual(result[2], expected[2], msg)
+        self.assertTrue(parsers_match(result[3], expected[3], range(0, 33)), msg)
+        self.assertEqual(result[4], expected[4], msg)
+        self.assertTrue(parsers_match(result[5], expected[5], range(-1, 101)), msg)
+
+
+    def test_pattern_suffix(self):
+        fmt = '%M/%D/%Y suffix...'
+        result = get_pattern(fmt)
+        expected = [int_parser(1, 12), '/', int_parser(1, 31), '/', int_parser(0, 99), ' suffix...']
+        msg = 'Format parsing should retain any suffix content.'
+        self.assertTrue(parsers_match(result[0], expected[0], range(0, 14)), msg)
+        self.assertEqual(result[1], expected[1], msg)
+        self.assertTrue(parsers_match(result[2], expected[2], range(0, 33)), msg)
+        self.assertEqual(result[3], expected[3], msg)
+        self.assertTrue(parsers_match(result[4], expected[4], range(-1, 101)), msg)
+        self.assertEqual(result[5], expected[5], msg)
+
+
+    def test_valid_value(self):
+        fmt = '%M/%D/%Y=%h:%m'
+        val = '01/01/01=01:01'
+
+        month, day, year, hours, mins = parse(get_pattern(fmt), val)
+        msg = 'Parse should return values according to a pattern.'
+
+        self.assertEqual(month, 1, msg)
+        self.assertEqual(day, 1, msg)
+        self.assertEqual(year, 1, msg)
+        self.assertEqual(hours, 1, msg)
+        self.assertEqual(mins, 1, msg)
+
+
+    def test_invalid_value(self):
+        fmt = '%M/%D/%Y'
+        val = 'JAN/0/2001'
+
+        month, day, year = parse(get_pattern(fmt), val)
+        msg = 'None should be returned for invalid values.'
+
+        self.assertEqual(month, None, msg)
+        self.assertEqual(day, None, msg)
+        self.assertEqual(year, None, msg)
+
+
+    def test_invalid_delim(self):
+        fmt = '%M/%D/%Y'
+        val = '01|01|01'
+
+        month, day, year = parse(get_pattern(fmt), val)
+        msg = 'None should be returned when value uses invalid formatting.'
+
+        self.assertEqual(month, None, msg)
+        self.assertEqual(day, None, msg)
+        self.assertEqual(year, None, msg)
+
+
+    def test_parse_with_prefix(self):
+        fmt = 'Prefix: %M/%D/%Y'
+        val = 'Prefix: 01/01/01'
+
+        month, day, year = parse(get_pattern(fmt), val)
+        msg = 'Parse should only handle data after the prefix.'
+
+        self.assertEqual(month, 1, msg)
+        self.assertEqual(day, 1, msg)
+        self.assertEqual(year, 1, msg)
+
+
+    def test_parse_with_suffix(self):
+        fmt = '%M/%D/%Y, suffix.'
+        val = '01/01/01, suffix.'
+
+        month, day, year = parse(get_pattern(fmt), val)
+        msg = 'Parse should only handle data behind the suffix.'
+
+        self.assertEqual(month, 1, msg)
+        self.assertEqual(day, 1, msg)
+        self.assertEqual(year, 1, msg)
+
+
+def parsers_match(parser: Callable, control: Callable, nums: range) -> bool:
+    """Test that the output of a parser matches that of a control parser for a range of inputs."""
+
+    match = True
+
+    for i in nums:
+        val = str(i)
+        result = parser(val)
+        expected = control(val)
+
+        if result != expected:
+            match = False
+
+    return match
+
+
+if __name__ == '__main__':
+    unittest.main()
